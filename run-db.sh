00989a4c336e934f6c6d959fdec697465ff508d3
vietsaclo <vietsaclo@gmail.com>
2023-06-14 09:43:21 +0700

    Update run db

--- run-db.sh
+++ run-db.sh
@@ -1,1 +1,12 @@
-sudo docker-compose --project-name app-nestjs-base  --env-file .env -f db-compose/MY_docker-compose.prod.yml up -d --build
+#!/bin/bash [run: ./run-db.sh]
+
+cp_dev="MY_docker-compose.dev.yml"
+cp_prod="MY_docker-compose.prod.yml"
+
+read -p "Enter to select mode [dev/prod]: " MODE
+
+if [[ "$MODE" == "prod" ]]; then
+  sudo docker-compose --project-name app-binance-bot  --env-file .env -f db-compose/$cp_prod up -d --build
+else
+  sudo docker-compose --project-name app-binance-bot  --env-file .env -f db-compose/$cp_dev up -d --build
+fi
